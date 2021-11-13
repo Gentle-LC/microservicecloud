ca9ea4be1170da10d9e20c006781c01cbe7d9664
Gentle_LC <lichaoyq@163.com>
2021-11-13 22:14:51 +0800

    父类工程创建，api创建

---
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author LC
+ * @version 1.0.0
+ * @ClassName Dept
+ * @Description
+ * @date 2021-11-13 21:53:00
+ */
+
+@Data
+public class Dept implements Serializable {
+    //主键
+    private Long  deptno;
+    //部门名称
+    private String  dname;
+    //来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+    private String  db_source;
+
+}
